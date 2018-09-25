6c1a4d9ede87f7300bd2df2d2877af6d97f9b48d
Hubertformin <hformin@gmail.com>
2018-09-25 19:59:04 +0100

    reports

--- src/js/reports-controller.js
+++ src/js/reports-controller.js
@@ -1,8 +1,43 @@
 app.controller('reportsCtr',($scope)=>{
-    var ctx = document.getElementById("myChart").getContext('2d');
-    var myLineChart = new Chart(ctx, {
-        type: 'line',
-        data: [5,10],
-        options: {showLines:true}
+    //var ctx = document.getElementById("myChart").getContext('2d');
+    var graph =  Morris.Area({
+        element: 'orderChart',
+        data: [
+            {x: '2018-10-03',y:76},
+            {x: '2018-10-02',y:28},
+            {x: '2018-10-01',y:23},
+            {x: '2018-09-30',y:46},
+            {x: '2018-09-29',y:13},
+            {x: '2018-09-28',y:23},
+            {x: '2018-09-27',y:36},
+            {x: '2018-09-26',y:33},
+            {x: '2018-09-25',y:20},
+            {x: '2018-09-24',y:23},
+        ],
+        xkey: 'x',
+        ykeys: ['y'],
+        labels: ['Orders'],
+        hideHover: 'auto',
+        resize: true,
+        lineColors:['#009688'],
+        behaveLikeLine:true
     });
+    $scope.update = ()=>{
+        graph.setData([
+            {x: '2018-10-03',y:76},
+            {x: '2018-10-02',y:28},
+            {x: '2018-10-01',y:23},
+            {x: '2018-09-30',y:46},
+            {x: '2018-09-29',y:28},
+            {x: '2018-09-28',y:23},
+            {x: '2018-09-27',y:36},
+            {x: '2018-09-26',y:33},
+            {x: '2018-09-25',y:20},
+            {x: '2018-09-24',y:23},
+        ])
+    }
+    //date strinf
+    $scope.toDate = (dt)=>{
+        return new Date(dt).toDateString();
+    }
 })
