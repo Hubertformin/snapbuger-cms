cbdfc50210c11666eff8defd3bd8e84a1f73df94
Hubertformin <hformin@gmail.com>
2018-10-10 00:30:19 +0100

    cleaning merge

--- src/js/reports-controller.js
+++ src/js/reports-controller.js
@@ -196,46 +196,6 @@ $scope.updateGraph = ()=>{
         }
         $scope.graphData.push(el);
     }
-<<<<<<< HEAD
-<<<<<<< HEAD
-    //area charts 
-    var graph =  Morris.Area({
-        element: 'orderChart',
-        data:$scope.graphData,
-        xkey: 'x',
-        ykeys: ['y'],
-        labels: ['Orders'],
-        hideHover: 'auto',
-        resize: true,
-        lineColors:['#009688'],
-        behaveLikeLine:true
-    });
-    $scope.update = ()=>{
-        graph.setData([
-            {x: '2018-10-03',y:76},
-            {x: '2018-10-02',y:28},
-            {x: '2018-10-01',y:23},
-            {x: '2018-09-30',y:46},
-            {x: '2018-09-29',y:28},
-            {x: '2018-09-28',y:23},
-            {x: '2018-09-27',y:36},
-            {x: '2018-09-26',y:33},
-            {x: '2018-09-25',y:20},
-            {x: '2018-09-24',y:23},
-        ])
-    }
-    //date strinf
-    $scope.toDate = (dt)=>{
-        return new Date(dt).toDateString();
-    }
-}  
- $scope.plotGraph();
-=======
-    graph.setData($scope.graphData)
-}
-
->>>>>>> version1.0.1
-=======
     $scope.graph.setData($scope.graphData)
 }
 //setInterval($scope.updateGraph,6000);
@@ -356,5 +316,4 @@ $scope.showOverview = ()=>{
 }
 
 
->>>>>>> version1.0.1
 })
